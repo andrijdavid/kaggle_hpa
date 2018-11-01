6c5ad5cef0bae26ed2777d7e571b224d8b279e41
CAPELLE Thomas <thomas.capelle@inria.fr>
2018-11-01 19:38:39 +0100

    added utils.py file with FocalLoss and f1 metric

---
+++ utils.py
@@ -0,0 +1,35 @@
+#helper functions
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+def f1(y_pred:Tensor, y_true:Tensor, thresh:float=0.5, beta:float=1, eps:float=1e-9, sigmoid:bool=True) -> Rank0Tensor:
+    beta2 = beta**2
+    if sigmoid: y_pred = y_pred.sigmoid()
+    y_pred = (y_pred>0.5).float()
+    y_true = y_true.float()
+    TP = (y_pred*y_true).sum(dim=1)
+    prec = TP/(y_pred.sum(dim=1)+eps)
+    rec = TP/(y_true.sum(dim=1)+eps)
+    res = (prec*rec)/(prec*beta2+rec+eps)*(1+beta2)
+    return res.mean()
+
+class FocalLoss(nn.Module):
+    def __init__(self, gamma=2):
+        super().__init__()
+        self.gamma = gamma
+        
+    def forward(self, input, target):
+        if not (target.size() == input.size()):
+            raise ValueError("Target size ({}) must be the same as input size ({})"
+                             .format(target.size(), input.size()))
+
+        max_val = (-input).clamp(min=0)
+        loss = input - input * target + max_val + \
+            ((-max_val).exp() + (-input - max_val).exp()).log()
+
+        invprobs = F.logsigmoid(-input * (target * 2.0 - 1.0))
+        loss = (invprobs * self.gamma).exp() * loss
+        
+        return loss.sum(dim=1).mean()
