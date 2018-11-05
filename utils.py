dee3281418d0cce8fe306e2c7c2b27ae6d50dfdb
Thomas Capelle <thomascapelle@gmail.com>
2018-11-05 21:48:40 +0000

    last ocmmit before kill

--- utils.py
+++ utils.py
@@ -1,5 +1,5 @@
 #helper functions
-
+import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -14,6 +14,29 @@ def f1(y_pred, y_true, thresh:float=0.5, beta:float=1, eps:float=1e-9, sigmoid:b
     rec = TP/(y_true.sum(dim=1)+eps)
     res = (prec*rec)/(prec*beta2+rec+eps)*(1+beta2)
     return res.mean()
+
+def f1_np(y_pred, y_true, threshold=0.5):
+    y_pred = (y_pred>threshold).astype(int)
+    TP = (y_pred*y_true).sum(1)
+    prec = TP/(y_pred.sum(1)+1e-7)
+    rec = TP/(y_true.sum(1)+1e-7)
+    res = 2*prec*rec/(prec+rec+1e-7)
+    return res.mean()
+
+def f1_n(y_pred, y_true, thresh, n, default=0.5):
+    threshold = default * np.ones(y_pred.shape[1])
+    threshold[n]=thresh
+    return f1_np(y_pred, y_true, threshold)
+
+def find_thresh(y_pred, y_true):
+    ths = []
+    for i in range(y_pred.shape[1]):
+        aux = []
+        for th in np.linspace(0,1,100):
+            aux += [f1_n(y_pred, y_true, th, i)]
+        ths += [np.array(aux).argmax()/100]
+    return np.array(ths)
+
 
 class FocalLoss(nn.Module):
     def __init__(self, gamma=2):
@@ -33,3 +56,4 @@ class FocalLoss(nn.Module):
         loss = (invprobs * self.gamma).exp() * loss
         
         return loss.sum(dim=1).mean()
+    
