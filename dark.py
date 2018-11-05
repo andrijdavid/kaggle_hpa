64cc81a323d6a69f92390e149354a7d0e710dbdb
Thomas Capelle <thomascapelle@gmail.com>
2018-11-05 13:31:38 +0000

    Added darknet model and dark.py helper functions

---
+++ dark.py
@@ -0,0 +1,36 @@
+#DARKNETS
+
+from fastai.layers import *
+from fastai.torch_core import *
+
+class ResLayer(nn.Module):
+    "Resnet style `ResLayer`"
+    def __init__(self, ni:int):
+        "create ResLayer with `ni` inputs"
+        super().__init__()
+        self.conv1=conv_layer(ni, ni//2, ks=1)
+        self.conv2=conv_layer(ni//2, ni, ks=3)
+
+    def forward(self, x): return x + self.conv2(self.conv1(x))
+
+class Darknet(nn.Module):
+    "https://github.com/pjreddie/darknet"
+    def make_group_layer(self, ch_in:int, num_blocks:int, stride:int=1):
+        "starts with conv layer - `ch_in` channels in - then has `num_blocks` `ResLayer`"
+        return [conv_layer(ch_in, ch_in*2,stride=stride)
+               ] + [(ResLayer(ch_in*2)) for i in range(num_blocks)]
+
+    def __init__(self, num_blocks:Collection[int], num_classes:int, nf=32):
+        "create darknet with `nf` and `num_blocks` layers"
+        super().__init__()
+        layers = [conv_layer(4, nf, ks=3, stride=1)]
+        for i,nb in enumerate(num_blocks):
+            layers += self.make_group_layer(nf, nb, stride=2-(i==1))
+            nf *= 2
+        layers += [nn.AdaptiveAvgPool2d(1), Flatten(), nn.Linear(nf, num_classes)]
+        self.layers = nn.Sequential(*layers)
+
+    def forward(self, x): return self.layers(x)
+
+def dark_small([1,2,4,4,3], 28, 32)
+def dark_53([1,2,8,8,4], 28, 32)
